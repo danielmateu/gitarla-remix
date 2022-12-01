1b8cfcd8d1872858a6bdee0c15f4dc7697601570
Daniel Mateu <danielmateu86@gmail.com>
2022-12-01 20:31:49 +0100

    Estilizando pagina nosotros

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,8 +1,10 @@
-
+import imagen from '../../public/img/nosotros.jpg'
+import styles from '~/styles/nosotros.css'
 
 export function meta() {
     return {
-        title: 'GuitarLA - Nosotros'
+        title: 'GuitarLA - Nosotros',
+        description: 'Venta de guitarras, blog de música y más'
     }
 }
 
@@ -10,7 +12,12 @@ export function links(){
     return [
         {
             rel: 'stylesheet',
-            href: '/nosotros'
+            href: styles
+        },
+        {
+            rel: 'preload',
+            href: imagen,
+            as: 'image'
         }
     ]
 }
@@ -18,7 +25,22 @@ export function links(){
 
 const Nosotros = () => {
     return (
-        <div>Nosotros</div>
+        <main className="contenedor nosotros">
+            <h2 className="heading">Nosotros</h2>
+            <div className="contenido">
+                <img src={imagen} alt="Imagen sobre nosotros" />
+                <div>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum veniam reiciendis id, debitis omnis officia consequatur nihil et impedit tempore. Earum quasi veritatis illum eligendi sit atque dolorem commodi iste.
+                    Soluta totam veniam dignissimos atque cum magni ex, tempora excepturi aut cupiditate rem ut commodi saepe ullam quos. Tenetur aperiam vel quos laudantium iure fugiat omnis sunt qui. Hic, dolorem.
+                    </p>
+                    <p>Odit explicabo quae facilis fugit aut impedit cumque ex dicta quisquam, et, laudantium excepturi ullam, similique reiciendis nemo tempore magni dolore eaque possimus dolorem omnis ducimus. Architecto dicta officia ipsam.
+                    Adipisci asperiores, inventore dolore esse voluptatum velit et laborum culpa unde impedit natus iste voluptas quos molestias non veniam necessitatibus incidunt suscipit quibusdam amet aliquam, libero reiciendis repellendus voluptatem? Ducimus?</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum veniam reiciendis id, debitis omnis officia consequatur nihil et impedit tempore. Earum quasi veritatis illum eligendi sit atque dolorem commodi iste.
+                    Soluta totam veniam dignissimos atque cum magni ex, tempora excepturi aut cupiditate rem ut commodi saepe ullam quos. Tenetur aperiam vel quos laudantium iure fugiat omnis sunt qui. Hic, dolorem.
+                    </p>
+                </div>
+            </div>
+        </main>
     )
 }
 
