bd94e32323050b330613fe36bf06cb74cd6d9fd7
Daniel Mateu <danielmateu86@gmail.com>
2022-12-01 17:05:57 +0100

    hojas de estilo añadidas

--- app/root.jsx
+++ app/root.jsx
@@ -1,6 +1,8 @@
 import {
-    Meta
+    Meta,
+    Links,
 } from '@remix-run/react'
+import styles from './styles/index.css'
 
 
 export function meta() {
@@ -11,6 +13,34 @@ export function meta() {
             viewport: 'width=device-width,initial-scale=1'
         }
     )
+}
+
+
+
+export function links(){
+    return [
+        {
+            rel: 'stylesheet',
+            href: 'https://csstools.github.io/normalize.css/11.0.0/normalize.css'
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com',
+            crossOrigin: 'true'
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.gstatic.com'
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Poppins&family=Roboto:wght@500&family=Supermercado+One&display=swap'
+        },
+        {
+            rel: 'stylesheet',
+            href: styles,
+        },
+    ]
 }
 
 export default function App() {
@@ -27,6 +57,7 @@ const Document = ({children}) => {
         <html lang="es">
             <head>
                 <Meta/>
+                <Links/>
             </head>
             <body>
                 {children}
