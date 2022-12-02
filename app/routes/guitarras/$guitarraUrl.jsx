8316e8a98376cd920c562bbbbd27c6e08a15ece5
Daniel Mateu <danielmateu86@gmail.com>
2022-12-02 12:56:47 +0100

    Añadiendo info meta dinamicamente + cambio en los styles

--- app/routes/guitarras/$guitarraUrl.jsx
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -1,14 +1,35 @@
 import { useLoaderData } from "@remix-run/react";
+import { Link } from "react-router-dom";
 import { getGuitarra } from "~/models/guitarras.server";
+import styles from "~/styles/guitarras.css"
 
+export function links() {
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
 
-export async function loader({params}){
+export function meta({data}) {
 
-    const {guitarraUrl} = params
+    
+
+    return {
+        title: `GuitarLA - ${data.data[0].attributes.nombre}`,
+        description: `Guitarras, venta de guitarras, guitarra ${data.data[0].attributes.nombre}}`
+
+    }
+}
+
+export async function loader({ params }) {
+
+    const { guitarraUrl } = params
     // console.log(guitarraUrl);
 
     const guitarra = await getGuitarra(guitarraUrl);
-    
+
 
     return guitarra
 }
@@ -16,10 +37,20 @@ export async function loader({params}){
 const Guitarra = () => {
 
     const guitarra = useLoaderData();
-    const {nombre, descripcion, imagen, precio} = guitarra.data[0].attributes;
+    const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes;
 
     return (
-        <div>$guitarraUrl</div>
+
+        <main className="contenedor guitarra">
+            <img src={imagen.data.attributes.url} alt={`Imagen de la guitarra de ${nombre}`} className="imagen" />
+            <div className="contenido">
+                <h3>{nombre}</h3>
+                <p className="texto">{descripcion}</p>
+                <p className="precio">€ {precio}</p>
+                <Link className="enlace enlaceInicio" to={`/tienda`}>Volver</Link>
+            </div>
+        </main>
+
     )
 }
 
