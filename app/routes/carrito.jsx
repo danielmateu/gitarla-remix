e93122d3179dd0e9beea67a037dcefc0e3f86d5a
Daniel Mateu <danielmateu86@gmail.com>
2022-12-13 16:33:29 +0100

    Eliminar error de hidratación con ClientOnly

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useOutletContext } from '@remix-run/react'
 import styles from '~/styles/carrito.css'
+import { ClientOnly } from 'remix-utils'
 
 export function links() {
     return [
@@ -24,74 +25,78 @@ const Carrito = () => {
     const { carrito, actualizarCantidad, eliminarGuitarra } = useOutletContext();
 
     useEffect(() => {
-        const calculoTotal = carrito.reduce((total, producto) => total + (producto.cantidad * producto.precio), 0 );
+        const calculoTotal = carrito.reduce((total, producto) => total + (producto.cantidad * producto.precio), 0);
         setTotal(calculoTotal)
     }, [carrito])
-    
+
 
     // console.log(carrito)
     return (
-        <main className="contenedor">
-            <h1 className="heading">Carrito de compras</h1>
+        <ClientOnly fallback={'cargando...'}>
+            {() => (
+                <main className="contenedor">
+                    <h1 className="heading">Carrito de compras</h1>
 
-            <div className="contenido">
-                <div className="carrito">
-                    <h2>Articulos</h2>
+                    <div className="contenido">
+                        <div className="carrito">
+                            <h2>Articulos</h2>
 
-                    {carrito?.length < 1 ? 'Carrito vacío' : (
-                        carrito?.map(producto => (
-                            <div
-                                key={producto.id}
-                                className='producto'
-                            >
-                                <div>
-                                    <img src={producto.imagen} alt={`Imagen del producto ${producto.nombre}`} />
-                                </div>
-                                <div>
-                                    <p className="nombre">{producto.nombre}</p>
-                                    <p>Cantidad: </p>
+                            {carrito?.length < 1 ? 'Carrito vacío' : (
+                                carrito?.map(producto => (
+                                    <div
+                                        key={producto.id}
+                                        className='producto'
+                                    >
+                                        <div>
+                                            <img src={producto.imagen} alt={`Imagen del producto ${producto.nombre}`} />
+                                        </div>
+                                        <div>
+                                            <p className="nombre">{producto.nombre}</p>
+                                            <p>Cantidad: </p>
 
-                                    <select
-                                        name=""
-                                        id=""
-                                        value={producto.cantidad} 
-                                        className="select"
-                                        onChange={ e => actualizarCantidad({ 
-                                            cantidad: +e.target.value,
-                                            id: producto.id
-                                        })}
-                                        >
-                                        <option value="1">1</option>
-                                        <option value="2">2</option>
-                                        <option value="3">3</option>
-                                        <option value="4">4</option>
-                                        <option value="5">5</option>
-                                    </select>
+                                            <select
+                                                name=""
+                                                id=""
+                                                value={producto.cantidad}
+                                                className="select"
+                                                onChange={e => actualizarCantidad({
+                                                    cantidad: +e.target.value,
+                                                    id: producto.id
+                                                })}
+                                            >
+                                                <option value="1">1</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                                <option value="4">4</option>
+                                                <option value="5">5</option>
+                                            </select>
 
-                                    <p className="precio">€ <span>{producto.precio}</span></p>
-                                    <p className="subtotal">Subtotal: € <span>{producto.cantidad * producto.precio}</span></p>
+                                            <p className="precio">€ <span>{producto.precio}</span></p>
+                                            <p className="subtotal">Subtotal: € <span>{producto.cantidad * producto.precio}</span></p>
 
-                                    <button 
-                                        type="button"
-                                        className="btn-eliminar"
-                                        onClick={() => eliminarGuitarra(producto.id)}
-                                        >eliminar</button>
+                                            <button
+                                                type="button"
+                                                className="btn-eliminar"
+                                                onClick={() => eliminarGuitarra(producto.id)}
+                                            >eliminar</button>
 
-                                </div>
-                            </div>
-                        ))
-                    )}
+                                        </div>
+                                    </div>
+                                ))
+                            )}
 
-                </div>
+                        </div>
 
 
-                <aside className="resumen">
-                    <h3>Resumen del pedido</h3>
-                    <p>Total a pagar: € {total}</p>
-                </aside>
-            </div>
+                        <aside className="resumen">
+                            <h3>Resumen del pedido</h3>
+                            <p>Total a pagar: € {total}</p>
+                        </aside>
+                    </div>
 
-        </main>
+                </main>
+            )}
+        </ClientOnly>
     )
 }
 
